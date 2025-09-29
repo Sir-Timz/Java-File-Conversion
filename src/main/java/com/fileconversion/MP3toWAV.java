72b647bf19cafd71ffad2a9eb6dd55c289e2c77f
Sir-Timz <timiokeyode@hotmail.co.uk>
2025-09-29 17:21:08 +0100

    MP3 to WAV converter working

--- src/main/java/com/fileconversion/MP3toWAV.java
+++ src/main/java/com/fileconversion/MP3toWAV.java
@@ -1,31 +1,52 @@
 package com.fileconversion;
 
 
+import org.apache.commons.io.FilenameUtils;
+
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class MP3toWAV {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    public static String fileName;
+
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException {
+        Scanner inputReader = new Scanner(System.in);
+        System.out.print("Enter the file path: ");
+        String path =  inputReader.nextLine();
+        File convertedFile = toWAV(getAudioStream(path), fileName);
     }
 
     public static AudioInputStream getAudioStream(String path) throws UnsupportedAudioFileException, IOException {
         File audioFile = new File(path);
-        audioFile.
-        AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
-        return audioStream;
+        setFileName(audioFile);
+        AudioInputStream encodedStream =  AudioSystem.getAudioInputStream(audioFile);
+        AudioFormat encodedFormat = encodedStream.getFormat();
+        AudioFormat decodedFormat = createDecodedFormat(encodedFormat);
+        AudioInputStream decodedStream = AudioSystem.getAudioInputStream(decodedFormat, encodedStream);
+        return decodedStream;
     }
 
-    public static AudioFormat getAudioFormat(AudioInputStream audioStream) {
-        AudioFormat audioFormat = audioStream.getFormat();
-        return audioFormat;
-    }
+
 
     public static File toWAV(AudioInputStream audioStream, String fileName ) throws IOException {
-        File WAVFile = new File("C:ConvertedFiles/Audio/" + fileName + ".wav");
+        File WAVFile = new File("C:/ConvertedFiles/Audio/" + fileName + ".wav");
+        if (!WAVFile.getParentFile().exists()) {
+            WAVFile.mkdirs();
+            WAVFile.createNewFile();
+        }
+        System.out.println(WAVFile.getAbsolutePath());
         AudioSystem.write(audioStream, AudioFileFormat.Type.WAVE, WAVFile);
         return WAVFile;
+    }
+
+    public static void setFileName(File file) {
+        fileName = FilenameUtils.getBaseName(file.getName());
+    }
+
+    public static AudioFormat createDecodedFormat(AudioFormat oldFormat) {
+        AudioFormat newFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, oldFormat.getSampleRate(), 16, oldFormat.getChannels(), oldFormat.getChannels() * 2, oldFormat.getSampleRate(), false);
+        return newFormat;
     }
 }
